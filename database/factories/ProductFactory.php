4c7a6d8712fd19752ac1cefa2fe0ed9ae5716ed0
ibrahemshabanah2022 <ibrahemshabanah2022@gmail.com>
2024-08-03 12:43:02 +0300

    fix product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -13,7 +13,7 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            'category_id' => $this->faker->numberBetween(1382, 1391), // Random number between 1 and 10
+            'category_id' => $this->faker->numberBetween(1, 10), // Random number between 1 and 10
             'name' => $this->faker->words(3, true),
             'description' => $this->faker->paragraph(),
             'price' => $this->faker->randomFloat(2, 1, 1000),
