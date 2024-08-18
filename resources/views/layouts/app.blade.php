682c217b5cb24d858e56c1b347ac5b2e2e6607b0
ibrahemshabanah2022 <ibrahemshabanah2022@gmail.com>
2024-08-18 18:32:31 +0300

    add dashboard template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,17 +9,58 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     @livewireStyles
 
-    <!-- Fonts -->
+    {{-- <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Additional CSS Links -->
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/vendor/css/core.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/vendor/css/theme-default.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/css/demo.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('adminpanel/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/vendor/libs/apex-charts/apex-charts.css') }}" /> --}}
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('adminpanel/assets/img/favicon/favicon.ico') }}" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/vendor/fonts/boxicons.css') }}" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/vendor/css/core.css') }}"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/vendor/css/theme-default.css') }}"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/css/demo.css') }}" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet"
+        href="{{ asset('adminpanel/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminpanel/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="{{ asset('adminpanel/assets/vendor/js/helpers.js') }}"></script>
+
+    <!-- Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <script src="{{ asset('adminpanel/assets/js/config.js') }}"></script>
+
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
 </head>
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        <livewire:layout.navigation />
+        {{-- <livewire:layout.navigation /> --}}
 
         <!-- Page Heading -->
         @if (isset($header))
@@ -35,6 +76,26 @@
             {{ $slot }}
         </main>
     </div>
+    <script src="{{ asset('adminpanel/assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('adminpanel/assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('adminpanel/assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('adminpanel/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+    <script src="{{ asset('adminpanel/assets/vendor/js/menu.js') }}"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ asset('adminpanel/assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+
+    <!-- Main JS -->
+    <script src="{{ asset('adminpanel/assets/js/main.js') }}"></script>
+
+    <!-- Page JS -->
+    <script src="{{ asset('adminpanel/assets/js/dashboards-analytics.js') }}"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
 </body>
 @livewireScripts
 
