e853d0f3b085e2c151ef821090e0958e199a3b06
ibrahemshabanah2022 <ibrahemshabanah2022@gmail.com>
2024-08-24 01:35:07 +0300

    add soft delete to products in dashboard

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'category_id',
@@ -22,6 +23,7 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
     public function wishlist()
     {
         return $this->hasMany(Wishlist::class);
